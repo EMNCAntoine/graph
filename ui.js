ca3f9bbb4e41cf38a6afbe2141fb2f2560640ac9
EMNCAntoine <126569349+EMNCAntoine@users.noreply.github.com>
2023-03-02 15:50:42 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -12,3 +12,26 @@ async function displayUI() {
     var content = document.getElementById('content');
     content.style = "display: block";
 }
+
+async function displayEvents() {
+  var events = await getEvents();
+  if (!events || events.value.length < 1) {
+    var content = document.getElementById('content');
+    var noItemsMessage = document.createElement('p');
+    noItemsMessage.innerHTML = `No events for the coming week!`;
+    content.appendChild(noItemsMessage)
+
+  } else {
+    var wrapperShowEvents = document.getElementById('eventWrapper');
+    wrapperShowEvents.style = "display: block";
+    const eventsElement = document.getElementById('events');
+    eventsElement.innerHTML = '';
+    events.value.forEach(event => {
+      var eventList = document.createElement('li');
+      eventList.innerText = `${event.subject} - From  ${new Date(event.start.dateTime).toLocaleString()} to ${new Date(event.end.dateTime).toLocaleString()} `;
+      eventsElement.appendChild(eventList);
+    });
+  }
+  var btnShowEvents = document.getElementById('btnShowEvents');
+  btnShowEvents.style = "display: none";
+}
